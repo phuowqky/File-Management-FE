e821024b064005e742435cf4580e57a192b2b2b3
phuowqky <phuongtruongky14012004@gmail.com>
2026-03-08 03:08:38 +0700

    commit UI login mock data

--- src/app/features/auth/login/login.component.ts
+++ src/app/features/auth/login/login.component.ts
@@ -6,13 +6,13 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
+
 export class LoginComponent implements OnInit {
+
   loginForm!: FormGroup
 
-  constructor(
-    private fb: FormBuilder
-  ) {
-    console.log("xem cos sinh ra hay khong");
+  constructor(private fb: FormBuilder) {
+    console.log("23");
 
     this.loginForm = this.fb.group({
       username: ['', Validators.required],
@@ -20,12 +20,41 @@ export class LoginComponent implements OnInit {
     })
 
   }
-
-  ngOnInit(): void {
-  }
+  ngOnInit(): void { }
 
   onSubmit() {
-    console.log(this.loginForm.value);
-  }
+    //B1 lay data tu form
+    const { username, password } = this.loginForm.value;
 
+    //B2 Ktra du lieu
+    if (username != 'admin' || password != "123456") {
+      this.loginForm.setErrors({ invalidLogin: true })
+    } else {
+      console.log("Login success");
+    }
+  }
 }
+
+// export class LoginComponent implements OnInit {
+//   loginForm!: FormGroup
+
+//   constructor(
+//     private fb: FormBuilder
+//   ) {
+//     console.log("23");
+
+//     this.loginForm = this.fb.group({
+//       username: ['', Validators.required],
+//       password: ['', Validators.required]
+//     })
+
+//   }
+
+//   ngOnInit(): void {
+//   }
+
+//   onSubmit() {
+//     console.log(this.loginForm.value);
+//   }
+
+// }
